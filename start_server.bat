e0557b07cc8e9751d2c8850d8c758378234a32ac
SEBAQ User <sebaq@example.com>
2026-04-08 15:37:39 +0200

    Add questions files

--- start_server.bat
+++ start_server.bat
@@ -1,6 +1,8 @@
 @echo off
+cd /d %~dp0
 echo Starting PWA Quiz App Server...
-echo Open your browser to: http://localhost:8000/اسئلة.html
+echo Opening browser at: http://localhost:8000/index.html
 echo Press Ctrl+C to stop the server
+start "" "http://localhost:8000/index.html"
 python -m http.server 8000
 pause
